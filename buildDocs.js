71a0ab311612d472fc067144b0082b92d1fc79cd
kevhender <kevhender@gmail.com>
2020-01-24 10:13:26 -0500

    Test

--- buildDocs.js
+++ buildDocs.js
@@ -2,7 +2,7 @@
 const fs = require('fs')
 
 const main = () => {
-  fs.writeFileSync('build/test.js', `test: ${new Date()}`, 'utf8');
+  fs.writeFileSync('build/test.js', `test1: ${new Date()}`, 'utf8');
 }
 
 main()
